fa3145ab96105b3ab5f9e66cea9d0d6c248ab977
admin <abc@xyz.com>
2023-09-22 17:09:21 +0530

    pkg test16

--- server_cfg.sh
+++ server_cfg.sh
@@ -1,7 +1,7 @@
 sudo yum install java-1.8.0-amazon-corretto-devel.x86_64 -y 
 sudo yum install git -y
 sudo yum install maven -y
-git clone https://github.com/dicogit/addressbook.git
+sudo git clone https://github.com/dicogit/addressbook.git
 if [ -d "addressbook" ];
 then
     echo "clone exist"
@@ -12,6 +12,6 @@ else
     cd /home/ec2-user/addressbook
     git checkout pipe
 fi
-#cd /home/ec2-user/addressbook
-mvn package
+sudo cd /home/ec2-user/addressbook
+sudo mvn package
 
