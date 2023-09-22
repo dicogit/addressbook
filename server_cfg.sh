ec8cd1993f8c1c8e03de90accc559c2dcfba76dd
admin <abc@xyz.com>
2023-09-22 16:19:54 +0530

    coorection

--- server_cfg.sh
+++ server_cfg.sh
@@ -2,7 +2,7 @@ sudo yum install java-1.8.0-amazon-corretto-devel.x86_64 -y
 sudo yum install git -y
 sudo yum install maven -y
 git clone https://github.com/dicogit/addressbook.git
-if [-d "addressbook"]
+if [ -d "addressbook" ]
 then
     echo "clone exist"
     cd /home/ec2-user/addressbook
