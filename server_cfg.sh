c45b48bceb37b3763971a6f7c9192299543660c1
admin <abc@xyz.com>
2023-09-29 19:46:21 +0530

    5

--- server_cfg.sh
+++ server_cfg.sh
@@ -1,6 +1,6 @@
-sudo yum install java-1.8.0-amazon-corretto-devel.x86_64 -y 
+sudo yum install java -y
 sudo yum install git -y
-sudo yum install maven -y
+#sudo yum install maven -y
 sudo git clone https://github.com/dicogit/addressbook.git
 if [ -d "addressbook" ];
 then
