ccd787a10f6e1e509e1998fdf49201e759845265
admin <abc@xyz.com>
2023-09-29 21:19:10 +0530

    14

--- server_cfg.sh
+++ server_cfg.sh
@@ -1,22 +1,14 @@
-sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
+sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
-sudo git clone https://github.com/dicogit/addressbook.git
 if [ -d "addressbook" ];
 then
-    echo "clone exist"
-    echo "go to addressbook"
-    cd /home/ec2-user/addressbook
-    echo "pipe pull"
-    git pull origin pipe
+  echo "repo is cloned and exists"
+  cd /home/ec2-user/addressbook
+  git pull origin pipe
 else
-    git clone https://github.com/dicogit/addressbook.git
-    echo "go to addressbook"
-    cd /home/ec2-user/addressbook
-    echo "pipe branch"
-    git checkout pipe
+  git clone https://github.com/dicogit/addressbook.git
+  cd /home/ec2-user/addressbook
+  git checkout pipe
 fi
-#sudo cd /home/ec2-user/addressbook
-#git checkout pipe
-mvn package
-
+mvn packag
