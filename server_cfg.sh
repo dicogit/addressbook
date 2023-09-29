cc63637dd345da7b25d7773fa43aff92c426ff3e
admin <abc@xyz.com>
2023-09-29 23:00:27 +0530

    22

--- server_cfg.sh
+++ server_cfg.sh
@@ -5,10 +5,11 @@ if [ -d "addressbook" ];
 then
     echo "repo is cloned and exists"
     cd /home/ec2-user/addressbook
+    git pull origin pipe
     git checkout pipe
 else
     git clone https://github.com/dicogit/addressbook.git
     cd /home/ec2-user/addressbook
     git checkout pipe
 fi
-    mvn package
+mvn package
