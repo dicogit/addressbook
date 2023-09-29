d983d568b94e26f34344cbb7f7b80dc4d085c121
admin <abc@xyz.com>
2023-09-29 21:40:00 +0530

    17

--- server_cfg.sh
+++ server_cfg.sh
@@ -3,12 +3,12 @@ sudo yum install git -y
 sudo yum install maven -y
 if [ -d "addressbook" ];
 then
-  echo "repo is cloned and exists"
-  cd /home/ec2-user/addressbook
-  git checkout pipe
+    echo "repo is cloned and exists"
+    cd /home/ec2-user/addressbook
+    git checkout pipe
 else
-  git clone https://github.com/dicogit/addressbook.git
-  cd /home/ec2-user/addressbook
-  git checkout pipe
+    git clone https://github.com/dicogit/addressbook.git
+    cd /home/ec2-user/addressbook
+    git checkout pipe
 fi
-mvn package
+    mvn package
